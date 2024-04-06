a3d38e2289667426a77301e31fad6ff9f0bb1745
pedro <pedro@MacBook-Pro-de-pedro.local>
2024-04-06 15:18:06 -0300

    refactor: add steps to create database

--- seeds/create-database-postgres/index.ts
+++ seeds/create-database-postgres/index.ts
@@ -5,26 +5,27 @@ import pgp from "pg-promise";
   const connection = pgp()(
     `postgres://postgres:postgres@localhost:5432/postgres`
   );
+  try {
+    const queries = [
+      { name: "drop schema", query: "drop schema if exists cccat16 cascade;" },
+      { name: "create schema", query: "create schema cccat16;" },
+      {
+        name: "create table account",
+        query:
+          "create table cccat16.account (account_id uuid primary key,name text not null,email text not null,cpf text not null,car_plate text null,is_passenger boolean not null default false,is_driver boolean not null default false);",
+      },
+    ];
 
-  await connection.query(
-    `
-    drop schema cccat16 cascade;
+    for (const query of queries) {
+      console.log("running: ", query.name);
+      await connection.query(query.query, []);
+    }
 
-    create schema cccat16;
-
-    create table cccat16.account (
-        account_id uuid primary key,
-        name text not null,
-        email text not null,
-        cpf text not null,
-        car_plate text null,
-        is_passenger boolean not null default false,
-        is_driver boolean not null default false
-    );
-    `,
-    []
-  );
-
-  await connection.$pool.end();
-  return;
+    console.log("Database initialized successfully");
+  } catch (error) {
+    console.log("erro ao rodar query \n\n", error);
+  } finally {
+    await connection.$pool.end();
+    return;
+  }
 })();
